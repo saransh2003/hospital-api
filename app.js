4f52122e5d636d0ac5760ef308d7c1916f16cc86
Saransh Yadav <62564143+saransh2003@users.noreply.github.com>
2023-08-06 19:19:21 +0530

    Update app.js
    Documentation from Postman Added.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use("/api", reportRoutes);
 
 app.get("/", (req, res) => {
   res.send(
-    '<div style = "font-family: Arial, Helvetica, sans-serif;display: flex; flex-direction:column; gap:2rem;align-items:center; justify-content:center; color:orangered; padding: 30px"><h1 style="font-size: 3rem">🏥 Hospital API 🚑</h1><a href="https://documenter.getpostman.com/view/28551869/2s946fdsQS" style = "background: orangered; padding: 10px 20px; color:white; text-decoration:none; font-size:1rem; cursor:pointer; border-radius:5px" href="/api-docs"><p style="font-size: 2rem; margin:0">Documentation 📄</p></div>'
+    '<div style = "font-family: Arial, Helvetica, sans-serif;display: flex; flex-direction:column; gap:2rem;align-items:center; justify-content:center; color:orangered; padding: 30px"><h1 style="font-size: 3rem">🏥 Hospital API 🚑</h1><a href="https://documenter.getpostman.com/view/28801098/2s9XxySE1V" style = "background: orangered; padding: 10px 20px; color:white; text-decoration:none; font-size:1rem; cursor:pointer; border-radius:5px" href="/api-docs"><p style="font-size: 2rem; margin:0">Documentation 📄</p></div>'
   );
 });
 
